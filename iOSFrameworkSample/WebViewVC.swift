0f49e9f4c48ee9d79c92dd79cac33e73c5e604d9
Kuldipkumar Prajapati <kuldipkumar.prajapati@borngroup.com>
2024-07-12 11:40:39 +0530

    first commit

---
+++ iOSFrameworkSample/WebViewVC.swift
@@ -0,0 +1,16 @@
+//
+//  WebViewVC.swift
+//  iOSFrameworkSample
+//
+//  Created by Kuldipkumar Prajapati on 12/07/24.
+//
+
+import Foundation
+
+public struct WebViewVC{
+    
+    public static func sayHello(messageString: String) -> String {
+        print(messageString)
+        return "Hello" + messageString;
+    }
+}
---
+++ iOSFrameworkSample/iOSFrameworkSample.h
@@ -0,0 +1,18 @@
+//
+//  iOSFrameworkSample.h
+//  iOSFrameworkSample
+//
+//  Created by Kuldipkumar Prajapati on 12/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSFrameworkSample.
+FOUNDATION_EXPORT double iOSFrameworkSampleVersionNumber;
+
+//! Project version string for iOSFrameworkSample.
+FOUNDATION_EXPORT const unsigned char iOSFrameworkSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSFrameworkSample/PublicHeader.h>
+
+
